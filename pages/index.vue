71a89a34a5528e42be143ee8d280d642e3e3ea49
nejjaoui <nejjaoui@gmail.com>
2020-11-22 12:39:16 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main text-center flex flex-col items-center justify-center">
     <!-- <logo /> -->
-    <h1 class="title">Nuxt — Tailwind — Netlify CMS</h1>
+    <h1 class="title">Nuxt — Tailwind — </h1>
     <h2 class="subtitle">Boilerplate</h2>
   </div>
 </template>
